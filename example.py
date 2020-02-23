bd8ec95a5c8764154e6f28f0e03f39f9db588134
Greg LaRocca <larocca89@gmail.com>
2020-02-23 12:37:02 -0500

    updated example.py
    Updated pull data in both JSON and CSV file formats.

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 from time_series_datapoints import *
 from wto_api_key import *
 
-hamburgers = get_time_series_datapoints('TP_A_0030', api_key, 'test')
+get_time_series_datapoints('TP_A_0030', api_key) #outputs a JSON file with the desired indicator
+get_time_series_datapoints('TP_A_0030', api_key, frmt='csv') #outputs a csv file with the desired indicator
